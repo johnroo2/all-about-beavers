18ffce88b46440367017b2977ba169b3453c2739
John Liu <johnliu@v1040-wn-rt-a-111-64.campus-dynamic.uwaterloo.ca>
2023-12-01 14:41:16 -0500

    inital commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,152 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useParallax } from "react-scroll-parallax";
 
-const inter = Inter({ subsets: ['latin'] })
+export const PolyWave = () => {
+  return(
+      <div className="polywave-wrapper polywave-animation pointer-events-none">
+          <div className="absolute z-0 w-full h-full bg-transparent"/>
+          <div className="polywave-inner">
+              <div className="bg-poly1 polywave polywave1"/>
+          </div>
+          <div className="polywave-inner">
+              <div className="bg-poly2 polywave polywave2"/>
+          </div>
+          <div className="polywave-inner">
+              <div className="bg-poly3 polywave polywave3"/>
+          </div>
+      </div>
+  )
+}
 
-export default function Home() {
+export default function Index() {
+  const ptitle = useParallax<any>({
+    scale: [2, 0.8],
+    translateY: ["-200px", "0px"]
+  }).ref
+
+  const p1 = useParallax<any>({
+    translateX: [-100, 100],
+    easing: [0, 1, 1, 0],
+  }).ref
+
+  const p2 = useParallax<any>({
+    translateX: [100, -100, [0, 1, 1, 0]],
+    opacity: [2, 0]
+  }).ref
+
+  const p3 = useParallax<any>({
+    translateX: [-100, 100],
+    easing: [0, 1, 1, 0],
+  }).ref
+
+  const p4 = useParallax<any>({
+    translateX: [100, -100],
+    easing: [0, 1, 1, 0]
+  }).ref
+
+  const pbg = useParallax<any>({
+    translateX: ["-150vw", "0vw"],
+  }).ref
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative background z-[0] max-w-[100vw] overflow-hidden">
+      <div className="absolute top-0 left-0 z-[1] flex flex-row items-start justify-start h-full
+      overflow-hidden">
+        <div ref={pbg} className="relative circles-img h-full"/>
+      </div>
+
+      <main className="relative z-[1] pb-[10rem] flex flex-col text-sky-50">
+        <div className="h-20 start"/>
+        <div className="relative z-[20] h-[500px]">
+          <h1 ref={ptitle} className="absolute text-7xl font-semibold w-full text-center mt-[300px] z-[100]">
+            🪵🦫 All About Beavers 🦫🪵
+          </h1>
+          <PolyWave/>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="h-[15rem] intermediate w-[500vw] mb-[5rem]"/>
+        <div ref={p1} className="flex flex-col 
+        shadow-xl items-center justify-center mb-[5rem] w-[100vw] gap-4 px-60">
+          <div className="text-3xl font-black">
+            About Beavers, Part 1 
+          </div>
+          <div className="text-lg">
+          Beavers (genus Castor) are large, semiaquatic rodents of the Northern Hemisphere. There are two existing species: the North American beaver (Castor canadensis) and the Eurasian beaver (C. fiber). Beavers are the second-largest living rodents, after capybaras, weighing up to 50 kg (110 lb). They have stout bodies with large heads, long chisel-like incisors, brown or gray fur, hand-like front feet, webbed back feet, and tails that are flat and scaly. The two species differ in skull and tail shape and fur color. Beavers can be found in a number of freshwater habitats, such as rivers, streams, lakes and ponds. They are herbivorous, consuming tree bark, aquatic plants, grasses and sedges.
+          </div>
+          <div className="text-lg">
+          Beavers build dams and lodges using tree branches, vegetation, rocks and mud; they chew down trees for building material. Dams restrict water flow, and lodges serve as shelters. Their infrastructure creates wetlands used by many other species, and because of their effect on other organisms in the ecosystem, beavers are considered a keystone species. Adult males and females live in monogamous pairs with their offspring. After their first year, the young help their parents repair dams and lodges; older siblings may also help raise newly-born offspring. Beavers hold territories and mark them using scent mounds made of mud, debris, and castoreum—a liquid substance excreted through the beaver's urethra-based castor sacs. Beavers can also recognize their kin by their anal gland secretions and are more likely to tolerate them as neighbors.
+          </div>
+        </div>
+        <div ref={p2} className="flex flex-col 
+        shadow-xl items-center justify-center mb-[5rem] w-[100vw]
+        px-60 gap-4 text-indigo-50">
+          <div className="text-3xl font-black">
+          About Beavers, Part 2
+          </div>
+          <div className="text-lg">
+          Historically, beavers have been hunted for their fur, meat, and castoreum. Castoreum has been used in medicine, perfume, and food flavoring; beaver pelts have been a major driver of the fur trade. Before protections began in the 19th and early 20th centuries, overhunting had nearly exterminated both species. Their populations have since rebounded, and they are listed as species of least concern by the IUCN Red List of mammals. In human culture, the beaver symbolizes industriousness, especially in connection with construction; it is the national animal of Canada.
+          </div>
+          <div className="text-lg">
+          The English word beaver comes from the Old English word beofor or befor and is connected to the German word biber and the Dutch word bever. The ultimate origin of the word is an Indo-European root for 'brown'. The genus name Castor has its origin in the Greek kastor and translates as 'beaver'. The name beaver is the source for several names of places in Europe including Beverley, Bièvres, Biberbach, Biebrich, Bibra, Bibern, Bibrka, Bobr, Bjurbäcker, Bjurfors, Bober, Bóbrka and Bjurlund.
+          </div>
+        </div>
+        <div ref={p3} className="flex flex-col shadow-xl items-center justify-center mb-[5rem] w-[100vw] gap-4">
+          <span className="text-4xl text-center font-semibold">Beavers: Image Gallery</span>
+          <div className="flex flex-row gap-4 p-8 rounded-md bg-white  max-w-[70%] overflow-x-scroll">
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/beaver3.jpeg" className="w-[200px] h-auto"/>
+          </div>
+        </div>
+        <div ref={p4} className="flex flex-col shadow-xl items-center justify-center mb-[5rem] w-[100vw] gap-4">
+          <span className="text-4xl text-center font-semibold">Beaver Dams: Images</span>
+          <div className="flex flex-row gap-4 p-8 rounded-md bg-white  max-w-[70%] overflow-x-scroll">
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam1.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam2.jpeg" className="w-[200px] h-auto"/>
+            <img src="/bdam3.jpeg" className="w-[200px] h-auto"/>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
